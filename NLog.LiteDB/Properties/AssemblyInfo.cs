9dfe045d64ccd010f28f2b5241f4536062e184fb
Doug Hunt <DHunt@cfigroup.com>
2018-09-01 10:13:01 -0400

    Add info data

--- NLog.LiteDB/Properties/AssemblyInfo.cs
+++ NLog.LiteDB/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NLog.LiteDb")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NLog Target for LiteDB database")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DougHnutPublishing LLC")]
 [assembly: AssemblyProduct("NLog.LiteDb")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © DougHnutPublishing LLC  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
