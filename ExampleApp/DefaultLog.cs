6bdd26ae64fe5e6f11c2ce62d7668f02897d3b18
Doug Hunt <DHunt@cfigroup.com>
2018-09-02 09:28:39 -0400

    Update spec with Files node

---
+++ ExampleApp/DefaultLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleApp
+{
+	public class DefaultLog
+	{
+		public int ThreadID { get; set; }
+		public string ThreadName { get; set; }
+		public int ProcessID { get; set; }
+		public string ProcessName { get; set; }
+		public string UserName { get; set; }
+		public string Level { get; set; }
+		public string Message { get; set; }
+	}
+}
---
+++ ExampleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NLog;
+
+namespace ExampleApp
+{
+	class Program
+	{
+		private readonly static Logger _logger = LogManager.GetCurrentClassLogger();
+		static void Main(string[] args)
+		{
+			_logger.Info($"Start of application at {DateTime.Now.ToString("HH.mm.ss")}");
+
+			DisplayLog();
+			Console.ReadLine();
+
+		}
+		static void DisplayLog()
+		{
+			var db = new LiteDB.LiteDatabase("filename=Example_Nlog.db");
+
+			var collection = db.GetCollection<DefaultLog>("DefaultLog");
+			Console.WriteLine($"There are {collection.Count()} log entries in collection");
+
+
+			var entries = collection.FindAll().ToList();
+
+			foreach (var item in entries)
+			{
+				Console.WriteLine($"{item.Level}  {item.Message}");
+			}
+
+		}
+	}
+}
